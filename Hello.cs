45984fec216e022dd088983b36ff1c0bedb74c88
Emzzy241 <emzzyoluwole@gmail.com>
2023-01-14 19:58:19 +0100

    adding my C# code File

---
+++ Hello.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // program code goes here
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Ahoy Mate");
+    }
+}
