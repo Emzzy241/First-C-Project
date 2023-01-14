32eef3d64b77de87ecb8d3a4d96bf73d837a1698
Emzzy241 <emzzyoluwole@gmail.com>
2023-01-14 20:06:37 +0100

    making new updates to my code file

--- Hello.cs
+++ Hello.cs
@@ -3,9 +3,13 @@ using System;
 class Program
 {
     static void Main()
+    // the Main() method is an ENTRY POINT and any method that will automatically run when a program is is launched is called an ENTRY POINT
     {
         // program code goes here
+        // Console.WriteLine() is for C# and Console.log() is for JavaScript
         Console.WriteLine("Hello World");
+        Console.WriteLine("Me First C# Project From Scratch");
         Console.WriteLine("Ahoy Mate");
+        Console.WriteLine("I Love C#");
     }
 }
